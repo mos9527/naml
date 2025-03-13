b7a8f1e3678f9e4129eac69cbba1b87498b890ff
mos9527 <greats3an@gmail.com>
2025-03-13 16:12:11 +0800

    Update

--- naml/modules/sequence.py
+++ naml/modules/sequence.py
@@ -1,6 +1,31 @@
 from naml.modules import torch, nn
+from naml.sequence import zero_one_mask
 
-
+class CELWithLengthMask(nn.CrossEntropyLoss):
+    '''Cross Entropy Loss with Length Mask
+```LOGITS                             TARGET
+[ # batch n                        [ # batch n               
+    [ # step m                         [ # step m            
+        [ logits for 0 ... k ]             index for 0 ... n 
+    ]                                  ]                     
+]                                  ]     
+CrossEntropyLoss expects           Returns with reduction=none
+[ # batch n                        [ # batch n                    
+    [ # class m                        [ # step m                 
+        [ # dimension k                    loss for 0 ... m       
+            logit at C[m,k]            ]                          
+        ]                          ]                              
+    ]                                                        
+]'''    
+    def __init__(self, *args, **kwargs):
+        super().__init__(reduction='none')        
+    def forward(self, logits: torch.Tensor, target: torch.Tensor, lens: torch.Tensor):        
+        loss = super().forward(logits.permute(0, 2, 1), target)
+        # Therefore a permute/transpose is needed
+        mask = zero_one_mask(loss.shape, lens)
+        loss *= mask
+        return loss.mean(dim=1)
+    
 class Seq2SeqEncoder(nn.Module):
     def __init__(self, n_vocab, n_embedding, n_hidden, n_layer, dropout_p):
         super().__init__()
